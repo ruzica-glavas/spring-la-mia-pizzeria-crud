e96fb02fa7631ea0971b4d83e45c5bfc3eb11a03
ruzica-glavas <ruzi99@hotmail.it>
2025-06-04 18:30:51 +0200

    first commit

---
+++ src/main/java/spring/la/mia/pizzeria/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -0,0 +1,12 @@
+package spring.la.mia.pizzeria.crud.spring_la_mia_pizzeria_crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pizza")
+
+
+public class PizzaController {
+    
+}
