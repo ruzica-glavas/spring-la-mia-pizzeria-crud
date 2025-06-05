08e089bf03b99bc224b87b24a35d3c9eafaab4a8
ruzica-glavas <ruzi99@hotmail.it>
2025-06-05 18:28:46 +0200

    add: finish step 1

--- src/main/java/spring/la/mia/pizzeria/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/spring/la/mia/pizzeria/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -1,8 +1,14 @@
 package spring.la.mia.pizzeria.crud.spring_la_mia_pizzeria_crud.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import spring.la.mia.pizzeria.crud.spring_la_mia_pizzeria_crud.model.Pizza;
+import spring.la.mia.pizzeria.crud.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 
 import org.springframework.ui.Model;
 
@@ -11,8 +17,16 @@ import org.springframework.ui.Model;
 
 
 public class PizzaController {
+
+    @Autowired
+    private PizzaRepository repository;
+
     @GetMapping
     public String index(Model model) {
+
+        List<Pizza> pizzas = repository.findAll();
+
+        model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
     }
 }
