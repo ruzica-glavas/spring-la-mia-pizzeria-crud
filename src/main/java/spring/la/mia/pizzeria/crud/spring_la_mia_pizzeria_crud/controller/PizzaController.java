c3107d1e6ac312ee7f4bec0a05132c51e22ff902
ruzica-glavas <ruzi99@hotmail.it>
2025-06-06 19:08:13 +0200

    add: CRUD update

--- src/main/java/spring/la/mia/pizzeria/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/spring/la/mia/pizzeria/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 
+
 @Controller
 @RequestMapping("/pizzas")
 
@@ -73,6 +74,25 @@ public class PizzaController {
         
         return "redirect:/pizzas";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "/pizzas/edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()){
+            return"/pizzas/edit";
+        }
+        
+        repository.save(formPizza);
+        return "redirect:/pizzas";
+    }
+    
+    
     
     
 }
