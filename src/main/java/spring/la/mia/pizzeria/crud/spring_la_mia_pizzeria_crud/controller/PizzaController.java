c4c217c113c3f4a980dc4931abaf3b470939bd38
ruzica-glavas <ruzi99@hotmail.it>
2025-06-06 11:18:13 +0200

    add: searchbar and a "go back" button

--- src/main/java/spring/la/mia/pizzeria/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/spring/la/mia/pizzeria/crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import spring.la.mia.pizzeria.crud.spring_la_mia_pizzeria_crud.model.Pizza;
 import spring.la.mia.pizzeria.crud.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
@@ -25,9 +26,15 @@ public class PizzaController {
     private PizzaRepository repository;
 
     @GetMapping
-    public String index(Model model) {
+    public String index(@RequestParam(name="name", required = false) String name,  Model model) {
 
-        List<Pizza> pizzas = repository.findAll();
+        List<Pizza> pizzas; //= repository.findAll();
+
+        if(name !=null && !name.isBlank()){
+            pizzas = repository.findByNameContainingIgnoreCase(name);
+        }else{
+            pizzas=repository.findAll();
+        }
 
         model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
